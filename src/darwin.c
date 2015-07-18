350669a98b85b546e5687119cc08e2c1bb5f78f4
Marcos <marcos.diez.garcia@gmail.com>
2015-07-18 12:56:56 +0200

    [+] Add basic darwin commandline options
    --help
    --version

--- src/darwin.c
+++ src/darwin.c
@@ -25,6 +25,7 @@
     #included
 
     <stdlib.h> free
+    <string.h> strcmp
     "hampath.h" "../../genome.h", load_hampath
     "nqueens.h" load_nqueens
     "subsetsum" load_subsetsum
@@ -37,13 +38,47 @@
     "experiment/parser_evolution.h" "../life.h", load_evolution
     "out/result.h" best_phenotype, write_result_csv
 */
+#ifdef HAVE_CONFIG_H
+    #include "../config.h"
+#endif /* HAVE_CONFIG_H */
+
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "experiment/hampath/parser_hampath.h"
 #include "experiment/nqueens/parser_nqueens.h"
 #include "experiment/subsetsum/parser_subsetsum.h"
 #include "experiment/parser_evolution.h"
 #include "out/result.h"
 
+
+void
+darwin_print_help(
+        void
+) {
+    printf("Usage: %s <evolution configuration file path>\n"
+            "\t\t<problem configuration file path> <results directory name>",
+                   PACKAGE);
+    printf("\n\n");
+    printf("Options:\n"
+            "--help       display this help and exit\n"
+            "--version    display the version information and exit");
+    printf("\n\n");
+    printf("Report bugs to: <%s>\n", PACKAGE_BUGREPORT);
+    printf("%s home page: <%s>\n", PACKAGE, PACKAGE_URL);
+    printf("General help using GNU software: <http://www.gnu.org/gethelp/>\n");
+}
+void
+darwin_print_version(
+        void
+) {
+    printf("%s\n", PACKAGE_STRING);
+    printf("%s\n", COPYRIGHT);
+    printf("%s comes with ABSOLUTELY NO WARRATY.\n", PACKAGE_NAME);
+    printf("You may redistribute copies of %s\n", PACKAGE_NAME);
+    printf("under the terms of the GNU General Public License.\n");
+    printf("For more information about these matters, see the file named COPYING\n");
+}
 
 #define DARWIN_DEFINE_FUNC_RUN(T, problem)                                  \
 void                                                                        \
@@ -70,7 +105,14 @@ run(                                                                        \
 
 int main(int argc, char **argv)
 {
-    run(argv[1], argv[2], argv[3]);
-
+    if (argc != 4) {
+        if ((argv[1] != NULL) && (strcmp(argv[1], "--version") == 0)) {
+            darwin_print_version();
+        } else {
+            darwin_print_help();
+        }
+    } else {
+        run(argv[1], argv[2], argv[3]);
+    }
     return 0;
 }
